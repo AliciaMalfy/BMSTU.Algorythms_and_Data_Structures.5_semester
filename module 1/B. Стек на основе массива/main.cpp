07f9b75f33022d9028b44deae45f0af132435e58
Alicia Malfy <37067064+AliciaMalfy@users.noreply.github.com>
2019-09-17 16:58:22 +0300

    Update main.cpp

--- module 1/B. Стек на основе массива/main.cpp
+++ module 1/B. Стек на основе массива/main.cpp
@@ -5,124 +5,114 @@ using namespace std;
 
 class Stack {
 private:
-	int capacity{};
-	int number{};
-	int count = 0;
-	int *mas{};
+    int capacity{};
+    int number{};
+    int count = 0;
+    int *mas{};
 
 public:
-	void set_size(int);
+    void set_size(int);
 
-	void push(int);
+    void push(int);
 
-	void pop();
+    void pop();
 
-	void print();
+    void print();
 };
 
 void Stack::set_size(int value) {
-	capacity = value;
-	mas = new int[capacity];
-
-	for (int i = 0; i < capacity; i++) {
-		mas[i] = -1;
-	}
+    capacity = value;
+    mas = new int[capacity];
 }
 
 void Stack::push(int value) {
-	number = value;
+    number = value;
 
-	if (count == capacity) {
-		cout << "overflow";
-	}
-	else {
-		mas[count] = number;
-		count++;
-	}
+    if (count == capacity) {
+        cout << "overflow";
+    } else {
+        mas[count] = number;
+        count++;
+    }
 }
 
 void Stack::pop() {
-	if (count == 0) {
-		cout << "underflow";
-	}
-	else {
-		mas[count] = -1;
-		count--;
-	}
+    if (count == 0) {
+        cout << "underflow" << endl;
+    } else {
+        count--;
+        cout << mas[count] << endl;
+    }
 }
 
 void Stack::print() {
-	if (count == 0) {
-		cout << "empty";
-	}
-	else {
-		for (int i = 0; i < capacity; i++) {
-			if (mas[i] >= -1) {
-				cout << mas[i] << " ";
-			}
-		}
-	}
+    if (count == 0) {
+        cout << "empty" << endl;
+    } else {
+        for (int i = 0; i < count; i++) {
+            cout << mas[i] << " ";
+        }
+    }
 }
 
 int definition(string str) {
-	string field = move(str);
-	int n = 0;
+    string field = move(str);
+    int n = 0;
 
-	if (field == "set_size") {
-		n = 1;
-	}
-	else if (field == "pop") {
-		n = 2;
-	}
-	else if (field == "push") {
-		n = 3;
-	}
-	else if (field == "print") {
-		n = 4;
-	}
+    if (field == "set_size") {
+        n = 1;
+    } else if (field == "pop") {
+        n = 2;
+    } else if (field == "push") {
+        n = 3;
+    } else if (field == "print") {
+        n = 4;
+    }
 
-	return n;
+    return n;
 }
 
 int main() {
-	string s, command;
-	int value, a;
-	Stack st;
-	stringstream ss;
+    string s, command;
+    int value, a, x, m;
+    Stack st;
+    stringstream ss;
 
-	for (;;) {
-		getline(cin, s);
+    while (getline(cin, s)) {
+        ss << s;
+        ss >> command;
+        ss >> value;
+        ss >> x;
 
-		ss << s;
-		ss >> command;
-		ss >> value;
+        if (x == 10) {
+            x = 0;
+            cout << "error" << endl;
+            goto t;
+        } else {
+            a = definition(command);
+        }
 
-		a = definition(command);
+        switch (a) {
+            case 1: {
+                st.set_size(value);
+                break;
+            }
+            case 2: {
+                st.pop();
+                t:
+                break;
+            }
+            case 3: {
+                st.push(value);
+                break;
+            }
+            case 4: {
+                st.print();
+                cout<<endl;
+                break;
+            }
+        }
 
-		switch (a) {
-		case 1: {
-			st.set_size(value);
-			break;
-		}
-		case 2: {
-			st.pop();
-			break;
-		}
-		case 3: {
-			st.push(value);
-			break;
-		}
-		case 4: {
-			st.print();
-			break;
-		}
-		default: {
-			cout << "Error";
-		}
-		}
-
-		ss.clear();
-
-		system("pause");
-	}
+        ss.clear();
+    }
 }
