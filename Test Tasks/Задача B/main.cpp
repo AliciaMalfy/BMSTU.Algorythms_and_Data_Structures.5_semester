876aeceda6ceed3691ccc4f38693e5be73f345ea
Alicia Malfy <37067064+AliciaMalfy@users.noreply.github.com>
2019-10-06 22:10:18 +0300

    Create main.cpp

---
+++ Test Tasks/Задача B/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+    string number, dop_str;
+    int sum = 0;
+    string str = ",";
+
+    while ((cin >> number)) {
+        if (number.empty()) {
+            continue;
+        }
+        for (int i = 0; i <= number.length(); i++) {
+            if (((number[i] >= '0') && (number[i] <= '9')) || number[i]=='-') {
+                dop_str += number[i];
+            } else {
+                sum += stoi(dop_str);
+                dop_str="";
+            }
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
