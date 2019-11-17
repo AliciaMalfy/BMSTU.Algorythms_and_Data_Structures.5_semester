71c95666c4b2195cd630c2e6dd78f56b7af0eb1c
Alicia Malfy <37067064+AliciaMalfy@users.noreply.github.com>
2019-11-17 23:36:14 +0300

    Update main.cpp

--- module 1/A. Задача про сумму/main.cpp
+++ module 1/A. Задача про сумму/main.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 int main() {
 
-    string number, dop_str;
-    int sum = 0;
-    string str = ",";
+	string number, dop_str;
+	int sum = 0;
 
-    while ((cin >> number)) {
-        for (int i = 0; i <= number.length(); i++) {
-            if (((number[i] >= '0') && (number[i] <= '9')) || number[i]=='-') {
-                dop_str += number[i];
-            } else {
-                sum += stoi(dop_str);
-                dop_str="";
-            }
-        }
-    }
+	while ((cin >> number)) {
+		for (int i = 0; i <= number.length(); i++) {
+			if (((number[i] >= '0') && (number[i] <= '9')) || number[i] == '-') {
+				dop_str += number[i];
+			}
+			else if (dop_str == "") {
+				sum += 0;
+			}
+			else {
+				sum += stoi(dop_str);
+				dop_str = "";
+			}
+		}
+	}
 
-    cout << sum;
+	cout << sum;
 
-    return 0;
+	return 0;
 }
